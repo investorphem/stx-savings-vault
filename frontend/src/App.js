160e02246c013b0fb4a0f7afd4c5d72ac96b368f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:38:32 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
     window.location.reload()
 
   const handleDeposit = async (
-    if (!userSession.isUserSignedIn()) return alert('Pleas  
+    if (!userSession.isUserSignedIn()) return alert('Pleas 
     setStatus('Depositing...')
 
     // Convert days to approximate blocks (approx 1 block every 0 mns)
