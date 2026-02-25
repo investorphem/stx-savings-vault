bfb6e6a099bcd54db8cf7e70b985c18f2c5d1dc8
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:32:36 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
   const disconnectWallet = () => {
     userSession.signUserOut()
     window.location.reload();
-  };
+  }
   const handleDeposit = async () => {
     if (!userSession.isUserSignedIn()) return alert('Please connect your wallet');
     setStatus('Depositing...');
