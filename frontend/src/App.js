be09e76fb8df43f4e2493886253310e1f8754642
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:40:52 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App()
   const [stxAmount, setStxAmount] = useState(0);
   const [lockDays, setLockDays] = useState(0);
   const [status, setStatus] = useState('Disconnected
-  const network = new StacksTestnet(); // Use StacksMainne formineeployment
+  const network = new StacksTestnet(); // Use StacksMainne frmineeployment
 
   const appDetails = {
     appName: "STX Savings Vault",
