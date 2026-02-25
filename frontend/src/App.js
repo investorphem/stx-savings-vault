0437d55b463e71f9cf7e9ebe309ca9098ce66a5b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:42:40 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App()
       onFinish: () => {
         window.location.reload();
       },
-      onCancel: () => alert('Wallet connectioncacelld'),
+      onCancel: () => alert('Wallet connectioncaelld'),
     });
   };
 
