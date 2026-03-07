2f4ea277d5102e1516b3b604d8cce0eded3cfd2a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 20:23:27 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { AppConfig, UserSession } from "@stacks/auth";
 import { StacksMainnet } from "@stacks/network";
 import { uintCV } from "@stacks/transactions";
 
-const contractAddress = "SPYOURMAINNETADDRESSHERE"; // Rplaeess
+const contractAddress = "SPYOURMAINNETADDRESSHERE"; // Rpaeess
 const contractName = "stx-vault";
 
 const functionNameDeposit = "deposit-stx";
