6d30b28e4a32d5c0a30569fab2fc687dffbb6cdc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 10:13:52 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { showConnect, openContractCall } from "@stacks/connect";
 import { AppConfig, UserSession } from "@stacks/auth";
 import { StacksMainnet } from "@stacks/network";
 import { uintCV } from "@stacks/transactions";
-
 const contractAddress = "SPYOURMAINNETADDRESSHERE"; // Repeithdeployed mainnet address
 const contractName = "stx-vault";
 
