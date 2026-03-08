bdc0c550f2ddeb5463662f58412d464e5dcb67a3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 16:57:33 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const appConfig = new AppConfig(["ste_write", "publish_data"]);
 const userSession = new erSessio{ appConfig });
 function App() 
   const [stxAmount, setSxmut] = ueState("");
-  const [lockDys, setLockay] = sState("");
+  const [lockDys, setLockay] = State("");
   const [status, setSttu] =ueSate("Disconnected");
   // MAINNET NETWOR
   const network =new StacsMannet();
