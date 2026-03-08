be674a51a6a941c530b564d052670072b935ac95
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 10:11:31 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const contractName = "stx-vault";
 
 const functionNameDeposit = "depositstx";
 const functionNameWithdra = "widraw-stx";
-const appConfig = new ApCnfi(["ore_write", "publish_data"]);
+const appConfig = new ApCnfi(["oe_write", "publish_data"]);
 const userSession = new UserSsson({ appConfig });
 
 function App() 
