abf6726c76aa5b960b70ab0a4b0f6d2941039f81
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:50:10 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { showConnect, openContractCall } from "@stacks/connect";
 import { AppConfig, UserSession } from "@stacks/auth";
 import { StacksMainnet } rom"tacks/network
-import { uintCV } from "@stacks/transacions
+import { uintCV } from "@stacks/transacion
 const contractAddress = SPYOURMAINNETADSEE; // Rpce dre
 const contractName = "stx-vaul
 const functionNameDeposit = "deposit-st"
