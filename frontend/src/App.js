1ce2ce574de51382f230f44def5fb0a817d55b85
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 08:15:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ const userSession = new UserSession({ appConfig });
 
 function App() {
   const [stxAmont, setStxAmount] = useState("");
-  const [lockDasetcys = useState("");
+  const [lockDasetcy = useState("");
   const [status, seStatus] = useState("Disconnected");
 
   // MAINNET NTWOR
