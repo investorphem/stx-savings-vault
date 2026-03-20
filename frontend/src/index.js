ca61cf0731c3449d0acd2131e56e582a07d2dec6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-20 23:34:28 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
-// frontend/src/index.js
+import { Buffer } from "buffer";
+// Standard polyfill for Stacks transactions in the browser
+window.Buffer = window.Buffer || Buffer;
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Optional: if you have a CSS file
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
