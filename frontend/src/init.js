7b183145fa1e6fba4aa5ebb0c65ff43ad30178cb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-21 12:05:44 +0100

    Update init.js

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,4 +2,10 @@ import { Buffer } from "buffer";
 
 if (typeof window !== "undefined") {
   window.Buffer = window.Buffer || Buffer;
+  window.global = window;
+  window.process = {
+    env: { DEBUG: undefined },
+    version: "",
+    nextTick: (cb) => setTimeout(cb, 0),
+  };
 }
