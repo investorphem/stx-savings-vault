8d3249256dd8a64bfd20b0d0688359bd24238dce
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 07:58:43 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,6 @@ import {
 // ==========================================
 const APP_VERSION = "11.1.0";
 const IS_MAINTENANCE = false;
-
 const ADMIN_ADDRESS = "SPYURMAINNETADDRESSHERE"; 
 const contractAddress = "SYOURMAINNETADDRESSHERE"; 
 const contractName = "stx-vault-v11"; // Upgraded to v11 for Iron Vault
