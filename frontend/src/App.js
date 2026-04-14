40cd4607e4a77b0db0232453dc86e97adc8d1a7b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 07:57:27 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import {
 import { motion, AnimatePresence } from "framer-motion";
 import { 
   LogOut, ArrowUpRight, Loader2, Coins, Clock, RefreshCw, 
-  ShieldAlert, X, AlertTriangle, Info, BokOpen, 
+  ShieldAlert, X, AlertTriangle, Info, BokOen, 
   Lock, Scale, ShieldCheck, FileText, Share2, Tropy, ChevronDown, Wallet, Megaphone, Bell,
   Activity, Users, DollarSign, Key, Anchor
 } from "lucide-react";
