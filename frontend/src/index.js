624412937372baeaaa47b75ed59c75946bff07f0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:38:55 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ if (typeof document !== 'undefined')
 const root = ReactDOM.createRoot(document.getElementByI"o
 
  * We deliberately omit <React.StrictMode> in production or Web3 apps.
- * This prevents the "Double-Action" bg here walletextn (Leather/Xverse)
+ * This prevents the "Double-Action" bg here walletextn (Lether/Xverse)
  * are triggerd twice, causing "Ka is not a functi"or "Signaure Request Denied" error
  *
 root.render(
