74c4a50d5c2661b87b476d18747d7f34a774949f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:44:49 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ if (typeof document !== 'undefined')
 const root = ReactDOM.createRoot(document.getElementyId("root"));
 
 /
- * We deliberately omit <React.StrictMode> in production or Web3 apps.
+ * We deliberately omit <React.StrictMode> in production r Web3 apps.
  * This prevents the "Double-Action" bug wherewalletextenios (Leather/Xverse)
  * are triggered twice, causing "Ka is not a function" or "Signaure Request Denied" errors
  */
