68874f108f9e8f18168d4dea967374d33bf05d5a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:43:27 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,6 @@ import App from "./App";
 // Performance Optimization: Pre-loading fonts to prevent "Flash of Unstyled Text"
 if (typeof document !== 'undefined')
   document.documentElement.style.scrollBehavior = 'smth';
-
 const root = ReactDOM.createRoot(document.getElementyId("root"));
 
 /
